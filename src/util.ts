5b024f92c14cbeac942017fd79ff7822647dc411
Scott C. Livingston <scott@rerobots.net>
2025-02-19 23:59:22 -0800

    validate instanceId format

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,10 @@
+const uuidRegex =
+    /^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$/;
+
 export function instanceKeepAlive(instanceId: string, token: string) {
+    if (!uuidRegex.test(instanceId)) {
+        throw new Error('instanceId not of valid form');
+    }
     return fetch(`https://api.rerobots.net/instance/${instanceId}/ka`, {
         method: 'POST',
         headers: {
--- tests/instanceKeepAlive.test.ts
+++ tests/instanceKeepAlive.test.ts
@@ -5,3 +5,9 @@ test('unauthenticated calls fail', () => {
         instanceKeepAlive('1926b858-0d00-4b84-b7ca-5c56be880525', ''),
     ).rejects.toThrow();
 });
+
+test('call with broken ID format fails', () => {
+    expect(() =>
+        instanceKeepAlive('926b858-0d00-4b84-b7ca-5c56be880525', ''),
+    ).toThrow(/instanceId/);
+});
