970e025afb5e0c0700d38d09dbf55ad701eed005
Scott C. Livingston <scott@rerobots.net>
2025-07-09 23:57:55 -0700

    prefer template literals

--- src/util.ts
+++ src/util.ts
@@ -8,7 +8,7 @@ export function instanceKeepAlive(instanceId: string, token: string) {
     return fetch(`https://api.rerobots.net/instance/${instanceId}/ka`, {
         method: 'POST',
         headers: {
-            Authorization: 'Bearer ' + token,
+            Authorization: `Bearer ${token}`,
             'Content-Type': 'application/json',
         },
     }).then((res) => {
